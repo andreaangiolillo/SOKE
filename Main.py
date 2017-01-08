7014427b9aba6f8bdd04dea7fd0049c5738b4256
andrew <andrew@luchy>
2017-01-08 21:26:13 +0100

    Implementazione SGDClassifier

--- Main.py
+++ Main.py
@@ -1,55 +1,68 @@
 import numpy as np
 from sklearn.utils.testing import assert_almost_equal
-from lightning.ranking import PRank
 import Preprocessing
+from sklearn.linear_model import SGDClassifier
 #from lightning.ranking import KernelPRank
 
-associations, evaluations = Preprocessing.extract_association_score(), Preprocessing.extract_user_evaluated_association()
-print "X: " + str(associations)
-print "y: "+str(evaluations)
 
-
-#y = np.round(y, decimals=-2)
-
-
-# TODO: Add more rankers.
-rankers = PRank(n_iter=10, shuffle=False, random_state=0),
-#            KernelPRank(kernel="sigmoid",
-#                    n_iter=10,
-#                        shuffle=True,
-#                        random_state=0))
-
-#Now we want to group the evaluations by user_id
-current_user_id = None
-data = []
-targets = []
-for evaluation in evaluations: #we learn from each user independently
-    if evaluation[0] == current_user_id:
-        targets.append(evaluation[2])
-        for association in associations: #we want to find the right row among the associations
-            if association[0] == evaluation[1]: #the two IDs are the same: we found the right row
-                data.append(association)
-                pass #exits this for loop without having to check all the associations
-    
-    else:
-        if data: #means: if data is not empty
-            data = np.array(data)
-            targets = np.array(targets)
-            for ranker in rankers:
-                print data
-                print targets
-                print ranker.__class__.__name__
-                ranker.fit(data, targets)
-                print ranker.score(data, targets)
-        current_user_id = evaluation[0]
-        data = []
-        targets = []
-        targets.append(evaluation[2])
-        for association in associations: 
-            if association[0] == evaluation[1]: 
-                data.append(association)
-                pass
-            
 
         
+#http://stackoverflow.com/questions/23056460/does-the-svm-in-sklearn-support-incremental-online-learning
+def linear_model() :
+    
+    associations, evaluations = Preprocessing.extract_association_score(), Preprocessing.extract_user_evaluated_association()
+    current_user_id = None
+    data = []
+    targets = []
+    for evaluation in evaluations: #we learn from each user independently
+        if evaluation[0] == current_user_id:
+            targets.append(evaluation[2])
+            for association in associations: #we want to find the right row among the associations
+                if association[0] == evaluation[1]: #the two IDs are the same: we found the right row
+                    data.append(association)
+                    pass #exits this for loop without having to check all the associations
+         
+        else:
+            if data: #means: if data is not empty
+                data = np.array(data)
+                targets = np.array(targets)
+#                 print "data"
+#                 print data
+#                 print "target"
+#                 print targets
+                
+                length = int(len(data)/5) #split data in 5 sets
+                j = 0
+                clf = SGDClassifier(loss="log", penalty="l2")
+                for i in (0, 4):
+                    print targets[j:length]
+                    clf.partial_fit(data[j:length],targets[j:length], classes=np.unique(targets))
+                    j = length + 1
+                    length += length 
+                #test    
+                print "y ", targets[j:length]
+                print "y_ ", clf.predict(data[j:length])#print predict
+         
+                 
+            current_user_id = evaluation[0]
+            data = []
+            targets = []
+            targets.append(evaluation[2])
+            for association in associations: 
+                if association[0] == evaluation[1]: 
+                    data.append(association)
+                    pass
+    
+    
+
+
+
+if __name__ == '__main__':
+    
+    linear_model()
+
+
+
+
+ 
         
