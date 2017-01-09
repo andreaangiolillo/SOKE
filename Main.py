98bdbb7297bc3b88990685fb9a504b5e1ff4ec89
thiswillbe <o.belingheri@campus.unimib.it>
2017-01-09 16:38:15 -0600

    WIP dictionary assoc_eval

--- Main.py
+++ Main.py
@@ -14,44 +14,71 @@ def linear_model() :
     current_user_id = None
     data = []
     targets = []
+    assoc_evals = {} #dictionary containing {association_score_id: "scores..scores.., target"}
+    current_user_id = evaluations[0][0]
     for evaluation in evaluations: #we learn from each user independently
         if evaluation[0] == current_user_id:
-            targets.append(evaluation[2])
-            for association in associations: #we want to find the right row among the associations
+            for association in associations:
                 if association[0] == evaluation[1]: #the two IDs are the same: we found the right row
-                    data.append(association)
-                    pass #exits this for loop without having to check all the associations
-         
+                    temp = []
+                    temp.append(associations[1:])
+                    temp.append(evaluation[2])
+                    assoc_evals[int(association[0])] = temp
+                    
         else:
-            if data: #means: if data is not empty
-                data = np.array(data)
-                targets = np.array(targets)
+            assoc_evals = {}
+            current_user_id = evaluation[0]
+            for association in associations:
+                if association[0] == evaluation[1]: #the two IDs are the same: we found the right row
+                    temp = []
+                    temp.append(associations[1:])
+                    temp.append(evaluation[2])
+                    assoc_evals[int(association[0])] = temp
+            
+         
+    print assoc_evals
+#         
+#         
+#         
+#         if evaluation[0] == current_user_id:
+#             targets.append(evaluation[2])
+#             for association in associations: #we want to find the right row among the associations
+#                 if association[0] == evaluation[1]: #the two IDs are the same: we found the right row
+#                     data.append(association)
+#                     continue #exits this for loop without having to check all the associations
+#          
+#         else:
+#             print "user" + str(evaluation[0])
+#             if data: #means: if data is not empty
+#                 data = np.array(data)
+#                 targets = np.array(targets)
+#                 print data
 #                 print "data"
 #                 print data
 #                 print "target"
 #                 print targets
-                
-                length = int(len(data)/5) #split data in 5 sets
-                j = 0
-                clf = SGDClassifier(loss="log", penalty="l2")
-                for i in (0, 4):
-                    print targets[j:length]
-                    clf.partial_fit(data[j:length],targets[j:length], classes=np.unique(targets))
-                    j = length + 1
-                    length += length 
+
+
+
+#         clf = SGDClassifier(loss="log", penalty="l2")
+#         clf.partial_fit(data[i],targets[i], classes=np.unique(targets))
+#         predictions = clf.predict(data[i+1: len(data)])
+#         d = {}
+#         for prediction in predictions:
+#             pass
+                    
                 #test    
-                print "y ", targets[j:length]
-                print "y_ ", clf.predict(data[j:length])#print predict
+
          
                  
-            current_user_id = evaluation[0]
-            data = []
-            targets = []
-            targets.append(evaluation[2])
-            for association in associations: 
-                if association[0] == evaluation[1]: 
-                    data.append(association)
-                    pass
+#             current_user_id = evaluation[0]
+#             data = []
+#             targets = []
+#             targets.append(evaluation[2])
+#             for association in associations: 
+#                 if association[0] == evaluation[1]: 
+#                     data.append(association)
+#                     pass
     
     
 
