e14e589138375905b5cce54c9794ed63db228bbb
lucky <lucky@lucky-N56VM>
2017-03-30 21:06:38 +0200

    adding some comment

--- Client_tcp.py
+++ Client_tcp.py
@@ -1,6 +1,6 @@
 import socket
 import pickle
-
+import numpy as np
 def main():
     host = "127.0.0.1"
     port = 5000
@@ -19,9 +19,14 @@ def main():
     data = s.recv(1024)
     data_arr = pickle.loads(data)#deserialization
     
+    
     print "Received from server:" 
     for i in data_arr:
         print i 
-
+    
+    #sending the valuation
+    eval = np.array[1,2,1,4,5,1] 
+    s.send(pickle.dumps(eval))
+        
 if __name__ == '__main__':
     main()
